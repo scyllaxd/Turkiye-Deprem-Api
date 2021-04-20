107aa9761c95652e3455b057145f879c9d8897fe
scyllaxd <50075629+scyllaxd@users.noreply.github.com>
2021-04-20 23:22:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ async function getirDepremler() {
 // 10 saniyede 1 islem yapma
 // setInterval(getirDepremler, 10000);
 
-app.get("/", function (req, res) {
-    res.render("index.ejs");
+app.get("/api", function (req, res) {
+    res.render("index.js");
 });
 
 /* Api Sayfasi */
